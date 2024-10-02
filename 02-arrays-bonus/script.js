b33c4afffda2665e8cea6e8281c06c6ae3b771ac
Gabbocnc <cianci.gabriele@hotmail.it>
2024-10-02 15:01:05 +0200

    complete task script 2

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -16,6 +16,13 @@ console.log(reversedTeachers);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+const longNames = teachers.filter(teachers => teachers.length >= 5)
+console.log(longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
+const index_Remove = teachers.indexOf('Ed');
+
+if (index_Remove !== -1) {
+  teachers.splice(index_Remove, 1);
+}
+console.log(teachers);

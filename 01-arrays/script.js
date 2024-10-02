0ac23422b7bea9fc4406a58da94b19b4c76b5d08
Gabbocnc <cianci.gabriele@hotmail.it>
2024-10-02 15:07:45 +0200

    edit task 8

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -10,7 +10,8 @@ const teachers = [
 
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
-
+const fourthTeacher = teachers[4];
+console.log(fourthTeacher);
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
 teachers.splice(5,1, "Patrick")
@@ -18,27 +19,35 @@ console.log(teachers);
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
 const lastTeacher = teachers.pop();
+
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
 const firstTeacher = teachers.shift();
 console.log(teachers);
+
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
 teachers.push('Vanessa');
 console.log(teachers);
+
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
 teachers.unshift('Sarah');
 console.log(teachers);
+
 // 7. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
 const isFabioPresent = 'Fabio';
 const exist = teachers.includes(isFabioPresent);
+
 // 8. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
-/* const lewisIndex = teachers.findIndex( 'Lewis' => teachers.length > 5);
-console.log(lewisIndex); */
+
+const lewisIndex = teachers.findIndex( teachers => teachers === 'Lewis');
+console.log(lewisIndex); 
+
 // 9. Unisci tutti gli insegnanti nell'array teachers in una stringa
 // separata da virgole e salvala nella variabile teachersString
 const teachersString = teachers.concat();
 console.log(teachersString);
+
 // 10. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
 const isTeachersEmpty = teachers.length;
